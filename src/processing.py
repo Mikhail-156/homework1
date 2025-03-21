a340de23b87be60eaa2c8b950a94d4f61475ccbb
Михаил Ергунов <ergunov.mih@mail.ru>
2025-03-21 16:37:10 +0500

    init

---
+++ src/processing.py
@@ -0,0 +1,12 @@
+def filter_by_state(bank_transaction: list[dict], state: str = "EXECUTED") -> list:
+    """Функция возврата банковских операций по ключу"""
+    state_bank_transaction = []
+    for item in bank_transaction:
+        if item["state"] == state:
+            state_bank_transaction.append(item)
+    return state_bank_transaction
+
+
+def sort_by_date(data_logs: list[dict], reverse: bool = True) -> list:
+    """Функция сортировке банковских операций по дате"""
+    return sorted(data_logs, key=lambda x: x["date"], reverse=reverse)
